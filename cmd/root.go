3d20dab9087cfdf57a98e96b48c2454c0593ca95
Kanishk Kaushik <kanishk.k@bluefunda.com>
2024-09-09 08:15:26 +0530

    root command trm

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "trm-cli",
+	Use:   "trm",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
